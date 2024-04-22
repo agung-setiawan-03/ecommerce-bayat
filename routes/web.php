43d9f3a6aadd34b5eb9d24f5644211333e6b9062
Agung Setiawan <agungrockstar6@gmail.com>
2024-04-22 09:52:12 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Frontend\UserController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+// User Route
+Route::get('user/dashboard',[UserController::class,'dashboard'])->middleware(['auth','role:user'])->name('user.dashboard');
+
+
+require __DIR__.'/auth.php';
